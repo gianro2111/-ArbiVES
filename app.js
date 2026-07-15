887656c33f516a382c53de5a9d7cdf3084dce165
gianro2111 <syrd7nfvrw@privaterelay.appleid.com>
2026-07-15 16:31:09 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 async function cargarBCV() {
   try {
-    // Temporal mientras conectamos la API definitiva
-    const tasaBCV = 724.00;
+    const respuesta = await fetch("https://pydolarve.org/api/v2/dollar?page=bcv");
+    const datos = await respuesta.json();
+
+    const tasaBCV = parseFloat(datos.monitors.usd.price);
 
     document.getElementById("bcv").innerText = tasaBCV.toFixed(2) + " Bs";
 
     calcular(tasaBCV, 860.00);
 
-  } catch (e) {
+  } catch (error) {
     document.getElementById("bcv").innerText = "Error";
   }
 }
-
-function calcular(bcv, usdt){
-
-    const comBanco=0.005;
-    const comBinance=0.043;
-    const comInternacional=0.015;
-
-    const costo=bcv*(1+comBanco)*(1+comBinance)*(1+comInternacional);
-
-    document.getElementById("usdt").innerText=usdt.toFixed(2)+" Bs";
-    document.getElementById("costo").innerText=costo.toFixed(2)+" Bs";
-
-    const rentabilidad=((usdt-costo)/costo)*100;
-
-    document.getElementById("ganancia").innerText=rentabilidad.toFixed(2)+" %";
-}
-
-cargarBCV();
