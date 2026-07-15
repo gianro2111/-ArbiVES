f8e33cd7a0cfd5dcf5d6dfecc687d88ab6d87b27
gianro2111 <syrd7nfvrw@privaterelay.appleid.com>
2026-07-15 17:37:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,29 @@
-const API_KEY = "ctz_live_9EWXK3NrVK0bU1LNgid8OWNdgIOp4OCESjKrbu";
+function calcular() {
 
-async function actualizar() {
+    const bcv = parseFloat(document.getElementById("bcv").value);
+    const usdt = parseFloat(document.getElementById("usdt").value);
 
-    try {
+    const comBanco = 0.5 / 100;
+    const comBinance = 4.3 / 100;
+    const comMaster = 1.5 / 100;
 
-        const respuesta = await fetch("https://api.cotizave.com/v1/fx/rates/reference", {
-  method: "GET",
-  headers: {
-    "X-API-Key": API_KEY,
-    "Accept": "application/json"
-  }
-});
-
-        const data = await respuesta.json();
-
-        const bcv = Number(data.mid);
-        const usdt = Number(data.usdt);
-
-        document.getElementById("bcv").innerText = bcv.toFixed(2)+" Bs";
-        document.getElementById("usdt").innerText = usdt.toFixed(2)+" Bs";
-
-        const costo = bcv * 1.005 * 1.043 * 1.015;
-
-        document.getElementById("costo").innerText =
-            costo.toFixed(2)+" Bs";
-
-        const rentabilidad =
-            ((usdt-costo)/costo)*100;
-
-        document.getElementById("ganancia").innerText =
-            rentabilidad.toFixed(2)+" %";
-
-    } catch(e){
-
-        console.log(e);
-
-        document.getElementById("bcv").innerText="Error";
-
+    if (isNaN(bcv) || isNaN(usdt)) {
+        alert("Ingrese BCV y USDT");
+        return;
     }
 
-}
+    const costo =
+        bcv *
+        (1 + comBanco) *
+        (1 + comBinance) *
+        (1 + comMaster);
 
-actualizar();
+    document.getElementById("costo").innerHTML =
+        costo.toFixed(2) + " Bs";
+
+    const rentabilidad =
+        ((usdt - costo) / costo) * 100;
+
+    document.getElementById("ganancia").innerHTML =
+        rentabilidad.toFixed(2) + " %";
+}
