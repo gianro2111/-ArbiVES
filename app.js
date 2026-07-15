64d3e19cfb9a7c3ae31b984d98f3bee8d69807ad
gianro2111 <syrd7nfvrw@privaterelay.appleid.com>
2026-07-15 17:09:42 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 async function cargarBCV() {
-  try {
-    const respuesta = await fetch("https://pydolarve.org/api/v2/dollar?page=bcv");
-    const datos = await respuesta.json();
+    try {
+        const res = await fetch("https://api.cotizave.com/v1/fx/rates/reference", {
+            headers: {
+                "X-API-Key":"ctz_live_9gzyzsQFICzYyCY56FV2pvO9Gl8v7EOTYyExcB",
+                "Accept": "application/json"
+            }
+        });
 
-    const tasaBCV = parseFloat(datos.monitors.usd.price);
+        const data = await res.json();
 
-    document.getElementById("bcv").innerText = tasaBCV.toFixed(2) + " Bs";
+        const bcv = data.mid;
 
-    calcular(tasaBCV, 860.00);
+        document.getElementById("bcv").innerHTML = bcv.toFixed(2) + " Bs";
 
-  } catch (error) {
-    document.getElementById("bcv").innerText = "Error";
-  }
+    } catch (e) {
+        document.getElementById("bcv").innerHTML = "Error";
+        console.log(e);
+    }
 }
+
+cargarBCV();
